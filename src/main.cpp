6d1f08421653ce330adcdf3884dd85de03c8f2ce
vinicius santos <viniciusangelus30@gmail.com>
2025-06-20 12:47:25 -0300

    eh

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <cmath>
 
 
+
 using namespace std;
 
 
@@ -14,35 +15,39 @@ class Pieces{
         vector<string> PosP;
         Texture2D T;
         map<string,Vector2> pgn;
-        map<string, string> pgnForPieces;
+        map<string, char> pgnForPieces;
         string piece;
-        
 
-        Pieces(vector<string> PosP, Texture2D T, map<string,Vector2> pgn, map<string, string> pgnForPieces,string piece){
+        Pieces(vector<string> PosP, Texture2D T, map<string,Vector2> pgn, map<string, char> pgnForPieces,char piece){
             this->PosP = PosP;
             this->T = T;
             this->pgn = pgn;
             this->pgnForPieces = pgnForPieces;
             this->piece = piece;
         }
-
         void DrawP(){
+            
             for(auto& cord : PosP){
                 DrawTexture(T, pgn[cord].x,pgn[cord].y,WHITE);
             }
         }
         
 
-
+        
         
 };
+
+Color YELLOW_LIGHT = {255, 255, 153, 255};
 
 
 int main(){
     
     string casa;
+    vector<string> casasAlvos;
+
+    char pieceForCase;
     map<string, Vector2> pgn;
-    map<string, string> pgnForPieces;
+    map<string, char> pgnForPieces;
 
 
     int telaX = 800;
@@ -67,31 +72,25 @@ int main(){
 
             pgn[key] = Vector2{ x, y };
             if(key[1] == '2' || key[1] == '7'){
-                pgnForPieces[key] = "peao";
+                pgnForPieces[key] = 'P';
             }else if(key == "A1" || key == "A8" || key == "H1" || key == "H8"){
-                pgnForPieces[key] = "torre";
+                pgnForPieces[key] = 'T';
             }else if(key == "B1" || key == "B8" || key == "G1" || key == "G8"){
-                pgnForPieces[key] = "cavalo";
+                pgnForPieces[key] = 'C';
             }else if(key == "C1" || key == "C8" || key == "F1" || key == "F8"){
-                pgnForPieces[key] = "bispo";
+                pgnForPieces[key] = 'B';
             }else if(key == "D1" || key == "D8"){
-                pgnForPieces[key] = "rainha";
+                pgnForPieces[key] = 'D';
             }else if(key == "E1" || key == "E8"){
-                pgnForPieces[key] = "rei";
+                pgnForPieces[key] = 'R';
             }else{
-                pgnForPieces[key] = "";
+                pgnForPieces[key] = '_';
             }
             
         }
     }
 
-    for(auto& p : pgn){
-        printf("%s -> (%.1f, %.1f)\n", p.first.c_str(), p.second.x, p.second.y);
-    }
- 
-    for(auto& PFP : pgnForPieces){
-        printf("%s>>%s\n", PFP.first.c_str(), PFP.second.c_str());
-    }
+    
 
     
     Image p = LoadImage("../imgs/pieces/white/pawn.png");
@@ -117,12 +116,12 @@ int main(){
     Texture2D TB = LoadTextureFromImage(B);
     Texture2D TT = LoadTextureFromImage(T);
 
-    Pieces Pawn({"A2","B2","C2","D2","E2","F2","G2","H2"}, Tp,pgn, pgnForPieces,"pawn");
-    Pieces Queen({"D1"},TQ,pgn, pgnForPieces,"queen");
-    Pieces King({"E1"},TK,pgn, pgnForPieces,"king");
-    Pieces vacalo({"B1","G1"},TH,pgn,pgnForPieces,"knight");
-    Pieces papa({"C1","F1"},TB,pgn,pgnForPieces,"bishop");
-    Pieces TorresGemeas({"A1","H1"},TT, pgn,pgnForPieces,"rook");
+    Pieces Pawn({"A2","B2","C2","D2","E2","F2","G2","H2"}, Tp,pgn, pgnForPieces,'P');
+    Pieces Queen({"D1"},TQ,pgn, pgnForPieces,'D');
+    Pieces King({"E1"},TK,pgn, pgnForPieces,'R');
+    Pieces vacalo({"B1","G1"},TH,pgn,pgnForPieces,'C');
+    Pieces papa({"C1","F1"},TB,pgn,pgnForPieces,'B');
+    Pieces TorresGemeas({"A1","H1"},TT, pgn,pgnForPieces,'T');
 
     //
     Image pBlack = LoadImage("../imgs/pieces/black/pawn.png");
@@ -146,14 +145,20 @@ int main(){
     Texture2D TBBlack = LoadTextureFromImage(BBlack);
     Texture2D TTBlack = LoadTextureFromImage(TBlack);
 
-    Pieces PawnBlack({"A7","B7","C7","D7","E7","F7","G7","H7"}, TpBlack, pgn,pgnForPieces,"pawn");
-    Pieces QueenBlack({"D8"}, TQBlack, pgn,pgnForPieces,"queen");
-    Pieces KingBlack({"E8"}, TKBlack, pgn,pgnForPieces,"king");
-    Pieces vacaloBlack({"B8","G8"}, THBlack, pgn,pgnForPieces,"knight");
-    Pieces papaBlack({"C8","F8"}, TBBlack, pgn,pgnForPieces,"bishop");
-    Pieces TorresGemeasBlack({"A8","H8"}, TTBlack, pgn,pgnForPieces,"rook");
+    Pieces PawnBlack({"A7","B7","C7","D7","E7","F7","G7","H7"}, TpBlack, pgn,pgnForPieces,'P');
+    Pieces QueenBlack({"D8"}, TQBlack, pgn,pgnForPieces,'D');
+    Pieces KingBlack({"E8"}, TKBlack, pgn,pgnForPieces,'R');
+    Pieces vacaloBlack({"B8","G8"}, THBlack, pgn,pgnForPieces,'C');
+    Pieces papaBlack({"C8","F8"}, TBBlack, pgn,pgnForPieces,'B');
+    Pieces TorresGemeasBlack({"A8","H8"}, TTBlack, pgn,pgnForPieces,'T');
 
-
+    for(auto& p : pgn){
+        printf("%s -> (%.1f, %.1f)\n", p.first.c_str(), p.second.x, p.second.y);
+    }
+ 
+    for(auto& PFP : pgnForPieces){
+        printf("%s>>%c\n", PFP.first.c_str(), PFP.second);
+    }
 
     while(!WindowShouldClose()){//MAIN LOOP!!!!
         BeginDrawing();
@@ -167,7 +172,51 @@ int main(){
         }
         if(!casa.empty()){
             DrawRectangle(pgn[casa].x,pgn[casa].y,100,100,YELLOW);
-        }
+            
+            switch(pieceForCase){
+                case 'P':
+                    if(casa[1] == '2'){
+
+                        auto it = pgn.find(casa);
+                        if (it != pgn.end()){
+                            it++;
+                            if(casasAlvos.size() >= 6){
+                                casasAlvos.clear();
+                            }else if(casasAlvos.size() <= 2){
+                                casasAlvos.push_back(it->first.c_str());
+                                
+                            }
+                            
+                            if(it != pgn.end()){
+                                DrawRectangle(it->second.x, it->second.y,100,100,YELLOW_LIGHT);
+                                it++;
+                                if(casasAlvos.size() <= 6){
+                                    casasAlvos.push_back(it->first.c_str());
+                                }
+                                if(it != pgn.end()){
+                                    DrawRectangle(it->second.x,it->second.y,100,100,YELLOW_LIGHT);
+                                }
+                            }
+                        }       
+                    }
+                    else if(casa[1] == '7'){
+                        auto it = pgn.find(casa);
+                        if (it != pgn.end()){
+                            it--;
+                            
+                            if(it != pgn.end()){
+                                DrawRectangle(it->second.x, it->second.y,100,100,YELLOW_LIGHT);
+                                it--;
+                                if(it != pgn.end()){
+                                    DrawRectangle(it->second.x,it->second.y,100,100,YELLOW_LIGHT);
+                                }
+                            }
+                        } 
+                    }
+                    break;
+            
+            }
+        }   
 
         Vector2 posMouse = GetMousePosition();
 
@@ -187,8 +236,6 @@ int main(){
         TorresGemeasBlack.DrawP();
         
         if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
-            
-            string piece;
             float x = posMouse.x;
             float y = posMouse.y;
 
@@ -198,11 +245,13 @@ int main(){
                 
                 if(abs((cx+50) - x) <= 50 && abs((cy + 50) - y) <= 50){
                     casa = cord.first.c_str();
-                    piece = pgnForPieces[casa];
+                    pieceForCase = pgnForPieces[casa];
                 }
+
             }
             
-            printf("click >> %s=%s \n", casa.c_str(),piece.c_str());
+            
+            printf("click >> %s=%c \n", casa.c_str(),pieceForCase);
             
         }
         
@@ -214,4 +263,7 @@ int main(){
     return 0;
 }
 
-//g++ main.cpp -o main.exe -I"D:/raylib/src" -L"D:/raylib/build/raylib" -lraylib -lopengl32 -lgdi32 -lwinmm
+//g++ main.cpp -o main.exe -I"D:/raylib/src" -L"D:/raylib/build/raylib" -lraylib -lopengl32 -lgdi32 -lwinmm {windows}
+// g++ main.cpp -o main -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 {linux}
+
+//g++ main.cpp -o /home/vinicius-pc/exe/chess/src/main -I//home/vinicius-pc/raylib/build/raylib/include/ -L/home/vinicius-pc/raylib/build/raylib/ -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
